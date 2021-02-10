f1130084b415b73cdfc4483a8c6b24683f0ff9bd
kristofer <kristofergauti@gmail.com>
2021-02-10 12:35:36 +0000

    Problems 1,2,3,4,5 and 6 finished but needs to be reviewed

--- QUERIES.sql
+++ QUERIES.sql
@@ -1,41 +1,62 @@
-CREATE DATABASE PII
 
-select * from Locations;
+--Problem 1
+SELECT locationid, location FROM Locations 
+ORDER BY location DESC;
 
--- select ...
+--Problem 2
+SELECT location FROM Locations
+WHERE caseCount >= 1
+ORDER BY location ASC;
 
-select 2 as Query;
+--Problem 3
+SELECT COUNT(name) FROM People P
+JOIN Genders G ON P.GenderId = G.GenderId
+WHERE gender = 'Female';
 
--- select ...
+-- Problem 4 Not Done
+SELECT name FROM People P 
+JOIN Agents A ON P.personid = A.secretidentity 
+JOIN Cases C ON C.agentid = A.agentid
+GROUP BY name HAVING count(name) > 10;
 
-select 3 as Query;
+--Problem 5
+SELECT P.personId, P.name, C.title FROM People P
+JOIN InvolvedIn I ON P.personId = I.personId
+JOIN Cases C ON I.CaseId = C.CaseId
+JOIN Locations L ON L.locationId = C.locationId
+WHERE I.isCulprit = true AND C.locationId = P.locationId;
 
--- select ...
 
-select 4 as Query;
+--Problem 6
+SELECT P.personId, P.name, G.gender FROM Genders G
+JOIN People P ON G.genderId = P.genderId
+JOIN Locations L ON L.locationId = P.locationId
+WHERE L.location = 'Selfoss';
 
--- select ...
+--Problem 7 Not finished
+SELECT * FROM People;
+SELECT * FROM cases;
+SELECT * FROM Locations;
+SELECT * FROM InvolvedIn;
 
-select 5 as Query;
+SELECT P.personId, P.name, Pr.description FROM Professions Pr
+JOIN People P ON Pr.professionId = P.professionId
+JOIN InvolvedIn I ON I.personId = P.personId
+JOIN Cases C ON C.caseId = I.caseId
+WHERE C.isclosed = false AND Pr.description = 'therapist';
 
--- select ...
+--Problem 8 Not finished
+SELECT A.codename, G.gender, P.password FROM Agents A
+JOIN Genders G ON G.GenderID = A.GenderID
+JOIN Passwords P ON P.AgentID = A.AgentID
+WHERE P.password LIKE '%' + A.codename +'%';
 
-select 6 as Query;
+-- SELECT ...
 
--- select ...
+SELECT 9 as Query;
 
-select 7 as Query;
+-- SELECT ...
 
--- select ...
+SELECT 10 as Query;
 
-select 8 as Query;
-
--- select ...
-
-select 9 as Query;
-
--- select ...
-
-select 10 as Query;
-
--- select ...
+-- SELECT ...
