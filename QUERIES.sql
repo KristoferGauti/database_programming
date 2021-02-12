3ac255cda0f88e5d48e96ddd9a130f2d5d2f6875
bergurtareqtamimi <bergurt19@ru.is>
2021-02-12 20:30:51 +0000

    finished piece of beauty

--- QUERIES.sql
+++ QUERIES.sql
@@ -1,5 +1,7 @@
 -- Problem 1
-SELECT 1 AS Query;
+SELECT
+    1 AS Query;
+
 SELECT
     locationid,
     location
@@ -9,7 +11,9 @@ ORDER BY
     location DESC;
 
 -- Problem 2
-SELECT 2 AS Query;
+SELECT
+    2 AS Query;
+
 SELECT
     location
 FROM
@@ -20,7 +24,9 @@ ORDER BY
     location ASC;
 
 -- Problem 3
-SELECT 3 AS Query;
+SELECT
+    3 AS Query;
+
 SELECT
     COUNT(name)
 FROM
@@ -30,7 +36,9 @@ WHERE
     gender = 'Female';
 
 -- Problem 4
-SELECT 4 AS Query;
+SELECT
+    4 AS Query;
+
 SELECT
     P.name
 FROM
@@ -43,11 +51,12 @@ WHERE
 GROUP BY
     P.name
 HAVING
-    COUNT(P.name) > 10
-
-
+    COUNT(P.name) > 10;
+    
 -- Problem 5
-SELECT 5 AS Query;
+SELECT
+    5 AS Query;
+
 SELECT
     P.personId,
     P.name,
@@ -62,36 +71,48 @@ WHERE
     AND C.locationId = P.locationId;
 
 -- Problem 6
-SELECT 6 AS Query;
 SELECT
-    P.personId,
+    6 AS Query;
+
+SELECT
+    DISTINCT P.personId,
     P.name,
     G.gender
 FROM
-    Genders G
-    JOIN People P ON G.genderId = P.genderId
-    JOIN Locations L ON L.locationId = P.locationId
+    Locations L
+    JOIN Cases C ON L.locationId = C.locationId
+    JOIN InvolvedIn I ON I.CaseID = C.caseId
+    JOIN People P ON P.personId = I.personId
+    JOIN Genders G ON G.genderId = P.genderId
 WHERE
     L.location = 'Selfoss';
 
-
 -- Problem 7
-SELECT 7 AS Query;
 SELECT
+    7 AS Query;
+
+SELECT
+    P.personId,
+    P.name,
     PR.description,
-    P.name
+    COUNT(P.personId) numcases 
 FROM
     Professions PR
     JOIN People P ON P.ProfessionID = PR.ProfessionID
     JOIN InvolvedIn I ON I.PersonID = P.PersonID
     JOIN Cases C ON C.CaseID = I.CaseID
 WHERE
-    PR.description LIKE '%therapist'
-    AND C.isClosed = FALSE;
-
+    PR.description LIKE '%therapist' AND
+    C.isClosed = 'false'
+GROUP BY
+    C.isClosed,
+    PR.description,
+    p.personId;
 
 --Problem 8
-SELECT 8 AS Query;
+SELECT
+    8 AS Query;
+
 SELECT
     A.codename,
     G.gender,
@@ -104,7 +125,9 @@ WHERE
     P.password LIKE CONCAT('%', A.codename, '%');
 
 -- Problem 9
-SELECT 9 AS Query;
+SELECT
+    9 AS Query;
+
 SELECT
     P.PersonID,
     P.name,
@@ -117,37 +140,30 @@ FROM
     JOIN InvolvedIn I ON P.PersonID = I.PersonID
     JOIN Cases C ON I.CaseId = C.CaseId
     JOIN Locations L ON C.LocationId = L.LocationId
-GROUP BY I.isCulprit, P.PersonId, L.LocationId
+GROUP BY
+    I.isCulprit,
+    P.PersonId,
+    L.LocationId
 HAVING
-     L.Location LIKE '%vogur' AND COUNT(C.caseId) >= 2;    
-
+    L.Location LIKE '%vogur'
+    AND COUNT(C.caseId) >= 2;
 
 -- Problem 10
-SELECT 10 AS Query;
-SELECT 
-    P.personId, 
-    P.name, 
-    G.gender, 
+SELECT
+    10 AS Query;
+
+SELECT
+    P.personId,
+    P.name,
+    G.gender,
     2045 - MAX(C.year) "yearsSinceLastInvestigation"
 FROM
-    People P 
+    People P
     JOIN InvolvedIn I ON I.personId = P.personId
-    JOIN Genders G ON G.genderId = P.genderId 
+    JOIN Genders G ON G.genderId = P.genderId
     JOIN Cases C ON C.caseId = I.caseId
-GROUP BY P.PersonId, G.gender
-HAVING COUNT(DISTINCT(I.agentId)) = 3; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+GROUP BY
+    P.PersonId,
+    G.gender
+HAVING
+    COUNT(DISTINCT(I.agentId)) = 3;
