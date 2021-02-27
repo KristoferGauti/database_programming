4ab392b480704d56c2c9ab75bc87e3e3dbaf2221
kristofer <kristofergauti@gmail.com>
2021-02-27 12:46:53 +0000

    Problem 1 fixed and completed

--- Group_project3/QUERIES.sql
+++ Group_project3/QUERIES.sql
@@ -1,5 +1,9 @@
+CREATE DATABASE PIII
+
+SELECT * FROM Locations
+
 SELECT
-    1 AS QUERY --The	PersonID,	name,	and	case	title	of	culprits	that	live	in	a	location	that	starts	with	the	
+    1 AS QUERY; --The	PersonID,	name,	and	case	title	of	culprits	that	live	in	a	location	that	starts	with	the	
     --same	letter	as	the	place	they	committed	their	crime.
 SELECT
     DISTINCT P.PersonID,
@@ -11,14 +15,11 @@ FROM
     JOIN Cases C ON I.CaseID = C.CaseID
     JOIN Locations L ON P.LocationId = L.LocationId
 WHERE
-    I.isCulprit = True
-SELECT
-    P.name
-From
-    People P
+    I.isCulprit = True AND LEFT(C.title, 1) = LEFT(L.location, 1)
 
 
-SELECT 2 AS QUERY 
+
+SELECT 2 AS QUERY; 
     
 -- The PersonID and name of women who are culprits and of men who are secret	
 -- identities of agents.
@@ -45,7 +46,7 @@ WHERE
     G.gender = 'Male'
 
 
-SELECT 3 AS QUERY 
+SELECT 3 AS QUERY; 
 --Find	the	codenames	of	double-agents.	A	double	agent	is	one	who	is a	culprit	in	a	case	
 --they	lead.
 SELECT
@@ -81,7 +82,7 @@ WHERE
 
 
 SELECT
-    4 AS QUERY 
+    4 AS QUERY; 
     --The	codename	and	designation	of	agents	who	have a license to kill	or	have	led	cases	
     --in	at	least	5	different	cities.
 
@@ -95,20 +96,20 @@ FROM
 
 
 SELECT
-    5 AS QUERY --The	codename,	secret	identity	name	and	designation	of	agents	who	have	closed	more	
+    5 AS QUERY; --The	codename,	secret	identity	name	and	designation	of	agents	who	have	closed	more	
     --cases	in	some	town	than	some	other	agent.
 SELECT
-    6 AS QUERY --The	code	name	and	designation	of	agents	who	lead	one	of	the	earliest	cases	in	some	
+    6 AS QUERY; --The	code	name	and	designation	of	agents	who	lead	one	of	the	earliest	cases	in	some	
     --location	(by	year),	and	have	only	lead	cases	in	one	other	location	(two	locations	total).
 SELECT
-    7 AS QUERY --Show	the	ID,	name	and	profession	of	People	who	have	been	involved	in	the	most	cases	
+    7 AS QUERY; --Show	the	ID,	name	and	profession	of	People	who	have	been	involved	in	the	most	cases	
     --in	each	location,	along	with	the	number	of	cases	they	have	been	involved	in	for	that	location,	
     --the	name	of	the	location	and	a	column	called	“secretly	agent?”	which	contains	1	if	the	person	
     --is	secretly	an	agent	or	0	if	the	person	is	not	an	agent.	If	you	can	print	‘yes’	and	‘no’	instead	of	
     --1	and	0,	all	the	better.
 SELECT
-    8 AS QUERY --The	designation	and	codename	of	agents	who	have	never	led a	case	in	“Akranes”
+    8 AS QUERY; --The	designation	and	codename	of	agents	who	have	never	led a	case	in	“Akranes”
 SELECT
-    9 AS QUERY --Show	the	ID,	title	and	location	of	all	cases	that	have	people	involved	of	all	genders.
+    9 AS QUERY; --Show	the	ID,	title	and	location	of	all	cases	that	have	people	involved	of	all	genders.
 SELECT
-    10 AS QUERY -- The	ID,	title	and	location	of	all	cases	that	have	no	known	people	involved	at	all
+    10 AS QUERY; -- The	ID,	title	and	location	of	all	cases	that	have	no	known	people	involved	at	all
